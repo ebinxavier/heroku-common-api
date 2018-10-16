a5babf4fc6e3e70c581aa91d3f3a97d101807be7
Ebin Xavier <ebinx7@gmail.com>
2018-10-16 15:23:53 +0530

    port fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-let port = process.ENV.PORT || 3000
+let port = process.env.PORT || 3030
 app.listen(port, () => {
     console.log("Listening to port",port);
 })
